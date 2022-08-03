9631ead026e056b482cd3131b9d24ab764a8f5c9
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-03 20:21:59 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/,message: "全角漢字かなカナで入力して下さい"}
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/,message: "全角漢字かなカナで入力して下さい"}
+         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナで入力して下さい"}
+         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナで入力して下さい"}
          validates :birthday, presence: true
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
 
          has_many :items
          has_many :buyers
