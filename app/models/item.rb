2ab38f2771b1eddbe68a0eccb83d1bf698108e84
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-04 21:41:00 +0900

    itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
   validates :postage_id, presence: true, numericality: { other_than: 1 }
   validates :area_id, presence: true, numericality: { other_than: 1 }
   validates :delivery_day_id, presence: true, numericality: { other_than: 1 }
-  validates :price, presence: true
+  validates :price, presence: true, numericality:{only_integer:true,greater_than_or_equal_to:300,less_than_or_equal_to:9999999}
   validates :image, presence: true
 
 end
