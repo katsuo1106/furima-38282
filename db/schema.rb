9ccae6b7f749362eceb8876b30aad6ebec537ef5
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-04 11:01:46 +0900

    itemモデル作成 マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_120707) do
+ActiveRecord::Schema.define(version: 2022_08_04_014538) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "item_name", null: false
+    t.text "item_description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "area_id", null: false
+    t.integer "delivery_day_id", null: false
+    t.integer "price", null: false
+    t.integer "timestamps"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +44,5 @@ ActiveRecord::Schema.define(version: 2022_08_02_120707) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
