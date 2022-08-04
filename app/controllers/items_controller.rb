ed60885f964c6039134c9b72982c66c809275249
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-04 16:32:37 +0900

    出品商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :postage_id, :area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+    
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :area
+  belongs_to :delivery_day
+
 
   belongs_to :user
   has_one :buyer
-  
+
   has_one_attached :image
+
+  validates :user, presence: true
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :postage_id, presence: true, numericality: { other_than: 1 }
+  validates :area_id, presence: true, numericality: { other_than: 1 }
+  validates :delivery_day_id, presence: true, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :image, presence: true
 
 end
