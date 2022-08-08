f6126d0d0546b92e6b2b35971b4a782f76ea65cb
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-08 11:41:42 +0900

    rails db:migrate

--- db/migrate/20220808021215_create_shopping_addresses.rb
+++ db/migrate/20220808021215_create_shopping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShoppingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shopping_addresses do |t|
-
+      t.references :buyer,null: false, foreign_key: true
+      t.string :post_code, null: false
+      t.integer :prefecture_id,null: false
+      t.string :city,null: false
+      t.string :address,null: false
+      t.string :building_name
+      t.integer :phone_number, null: false
       t.timestamps
     end
   end
