3907adfd724702194b38c47ad03fde68e02dd80f
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-09 17:07:21 +0900

    リファレンス

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,12 +1,13 @@
 class BuyersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create] 
+  before_action :move_to_index, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @buyer_shopping_address = BuyerShoppingAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_shopping_address = BuyerShoppingAddress.new(buyer_params)
     if @buyer_shopping_address.valid?
       pay_item
@@ -21,6 +22,14 @@ class BuyersController < ApplicationController
 
   def buyer_params
     params.require(:buyer_shopping_address).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id || @item.buyer.present?
   end
 
   def pay_item
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless @item.user_id == current_user.id
+    redirect_to action: :index unless current_user.id == @item.user_id && @item.buyer.nil?
   end
 end
